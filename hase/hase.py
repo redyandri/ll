b247cf1340ce710377afffe971c7176c1cad93f9
redyandri <redyandri@gmail.com>
2018-04-20 13:32:14 +0200

    20.4.2018 man crafted knn, exhaustive and INEFFICIENT :(

--- hase/hase.py
+++
@@ -1,6 +0,0 @@
-from mnist import MNIST
-import random
-
-mndata=MNIST("mnist")
-train_image,train_label=mndata.load_training()
-print mndata.display(train_image[random.randrange(0,len(train_image))])
--- hase/hist_display.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import sys
-sys.path.insert(0, "/home/andri/Documents/s2/5/master_arbeit/app")
-import RLXYMSH
-
-r=RLXYMSH()
-hist=r.get_rlbwxh("chess.png")
-print hist
